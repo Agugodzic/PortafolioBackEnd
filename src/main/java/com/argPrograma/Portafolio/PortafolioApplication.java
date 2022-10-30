c15534e5413c39d618273baf58d37ec258e618f1
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2022-10-30 19:31:37 -0300

    Update PortafolioApplication.java

--- src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
+++ src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
@@ -35,7 +35,7 @@ public class PortafolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portafolio-frontend-f5c86.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portafolio-frontend-f5c86.web.app","http://localhost:4200"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
