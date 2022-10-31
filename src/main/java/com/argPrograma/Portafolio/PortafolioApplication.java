e4bb7a0675ab59976bdd0ee6b6507f739d043ef5
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2022-10-31 08:42:33 -0300

    ..

--- src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
+++ src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
@@ -19,11 +19,23 @@ public class PortafolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("/**").allowedOrigins("http://portafolio-frontend-f5c86.web.app");
+			}
+		};
+	}
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://portafolio-frontend-f5c86.web.app","http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portafolio-frontend-f5c86.web.app","http://localhost:4200"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
