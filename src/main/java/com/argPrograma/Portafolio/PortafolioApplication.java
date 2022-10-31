2f955f1a75e0d0f6c47391582af0692ad30efff6
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2022-10-31 20:52:22 -0300

    Update PortafolioApplication.java

--- src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
+++ src/main/java/com/argPrograma/Portafolio/PortafolioApplication.java
@@ -24,7 +24,7 @@ public class PortafolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://portafolio-frontend-f5c86.web.app/").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
