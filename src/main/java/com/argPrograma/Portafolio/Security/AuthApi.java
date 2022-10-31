bfd1433f050e3a3edcc2445d3a8dd1234bfdf56d
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2022-10-31 09:14:12 -0300

    Update AuthApi.java

--- src/main/java/com/argPrograma/Portafolio/Security/AuthApi.java
+++ src/main/java/com/argPrograma/Portafolio/Security/AuthApi.java
@@ -23,7 +23,6 @@ public class AuthApi {
     @Autowired
     JwtTokenUtil jwtTokenUtil;
     @PostMapping("/login")
-    @CrossOrigin("https://portafolio-frontend-f5c86.web.app")
     public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request){
         try{
             Authentication authentication = authManager.authenticate(
